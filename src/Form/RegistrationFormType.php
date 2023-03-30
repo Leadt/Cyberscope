5f4894cb9e4e9cfd620cfcb6796f38ac61c37135
Lea Drouet <ledrouet@ensc.fr>
2023-03-30 15:31:50 +0200

    front inscription et connexion
    manque l'ajout de la photo à la base de données

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -3,16 +3,18 @@
 namespace App\Form;
 
 use App\Entity\User;
-use Doctrine\DBAL\Types\TextType;
-use Symfony\Component\Form\FormTypeInterface;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
-use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\FormTypeInterface;
 use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\IsTrue;
 use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\Image;
 use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
 
 
 
@@ -21,17 +23,58 @@ class RegistrationFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            /*->add('pseudo_user', TextType::class, ['label' => 'Pseudo'])
-            ->add('nom_user', TextType::class, ['label' => 'Nom'])
-            ->add('prenom_user', TextType::class, ['label' => 'Prénom'])*/
-            ->add('pseudo_user')
-            ->add('nom_user')
-            ->add('prenom_user')
+            ->add('pseudo_user', TextType::class, [
+                'label' => false,
+                'attr' => ['placeholder' => 'Pseudo'],
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Veuillez entrez un pseudo',
+                    ]),
+                    new Length([
+                        'min' => 3,
+                        'minMessage' => 'Le pseudo doit contenir au moins 3 caratères',
+                        'max' => 180,
+                    ]),
+                ],
+
+            ])
+            ->add('nom_user', TextType::class, [
+                'label' => false,
+                'attr' => ['placeholder' => 'Nom'],
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Veuillez entrez un nom',
+                    ]),
+                    new Length([
+                        'min' => 2,
+                        'minMessage' => 'Le nom doit contenir au moins 2 caratères',
+                        'max' => 30,
+                    ]),
+                ],
+
+            ])
+            ->add('prenom_user', TextType::class, [
+                'label' => false,
+                'attr' => ['placeholder' => 'Prénom'],
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Veuillez entrez un prénom',
+                    ]),
+                    new Length([
+                        'min' => 2,
+                        'minMessage' => 'Le prénom doit contenir au moins 2 caratères',
+                        'max' => 30,
+                    ]),
+                ],
+
+            ])
             ->add('plainPassword', PasswordType::class, [
                 // instead of being set onto the object directly,
                 // this is read and encoded in the controller
-                'mapped' => false,
-                'attr' => ['autocomplete' => 'new-password'],
+                'label' => false, 'mapped' => false,
+
+
+                'attr' => ['autocomplete' => 'new-password', 'placeholder' => 'Mot de passe'],
                 'constraints' => [
                     new NotBlank([
                         'message' => 'Veuillez entrez un mot de passe',
@@ -40,8 +83,15 @@ class RegistrationFormType extends AbstractType
                         'min' => 6,
                         'minMessage' => 'Le mot de passe doit contenir au moins 6 caratères',
                         // max length allowed by Symfony for security reasons
-                        'max' => 4096,
+                        'max' => 20,
                     ]),
+                ],
+            ])
+            ->add('photo_user', FileType::class, [
+                'required' => false,
+                'mapped' => false,
+                'constraints' => [
+                    new Image(['maxSize' => '1024k'])
                 ],
             ]);
     }
