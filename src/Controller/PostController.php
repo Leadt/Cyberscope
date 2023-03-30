c6e5750daa834c031d1559a23f9f1a7727c9e43c
Lea Drouet <ledrouet@ensc.fr>
2023-03-30 17:22:22 +0200

    form post 2

--- src/Controller/PostController.php
+++ src/Controller/PostController.php
@@ -3,10 +3,12 @@
 namespace App\Controller;
 
 use App\Entity\Post;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Form\PostFormType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use App\Form\PostFormType;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class PostController extends AbstractController
 {
@@ -19,11 +21,18 @@ class PostController extends AbstractController
     }
 
     #[Route('/formPost', name: "app_formPost")]
-    public function formPost(): Response
+    public function formPost(Request $request, EntityManagerInterface $entityManager): Response
     {
         $post = new Post();
         $form = $this->createForm(PostFormType::class, $post);
+        $form->handleRequest($request);
 
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($post);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_homepage');
+        }
         return $this->render('post/formPost.html.twig', [
             'Postform' => $form->createView()
         ]);
